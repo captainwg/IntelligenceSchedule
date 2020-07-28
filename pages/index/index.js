8b4659ef53548179029eaa704162a1c5f8b51d6b
wanggang <924706932@163.com>
2020-07-28 09:50:49 +0800

    低调
    的味道

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,7 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
+    motto1: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
